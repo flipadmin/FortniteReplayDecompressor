3dca7726cc94c58a3c51d453716b628f9517e2ec
Dmitriy Polomoshnov <dmitriy.polomoshnov@eeg.tech>
2024-11-04 12:33:12 +0300

    pack release

--- pack-eeg.cmd
+++ pack-eeg.cmd
@@ -1,5 +1,5 @@
 rem dotnet build -c Release .\src\FortniteReplayReader\FortniteReplayReader.csproj
 
-dotnet pack .\src\Unreal.Encryption\Unreal.Encryption.csproj -p:PackageVersion=2.2.0-eeg
-dotnet pack .\src\Unreal.Core\Unreal.Core.csproj -p:PackageVersion=2.2.0-eeg
-dotnet pack .\src\FortniteReplayReader\FortniteReplayReader.csproj -p:PackageVersion=2.2.0-eeg
+dotnet pack -c Release .\src\Unreal.Encryption\Unreal.Encryption.csproj -p:PackageVersion=2.2.0-eeg
+dotnet pack -c Release .\src\Unreal.Core\Unreal.Core.csproj -p:PackageVersion=2.2.0-eeg
+dotnet pack -c Release .\src\FortniteReplayReader\FortniteReplayReader.csproj -p:PackageVersion=2.2.0-eeg
